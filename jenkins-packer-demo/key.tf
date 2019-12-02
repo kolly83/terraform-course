f9fcddde511c8e4819b80477a141d814478de5b0
kolly83 <kolly22@gmail.com>
2019-12-02 08:56:20 +0000

    Update key.tf

--- jenkins-packer-demo/key.tf
+++ jenkins-packer-demo/key.tf
@@ -1,7 +1,4 @@
 resource "aws_key_pair" "mykeypair" {
   key_name = "mykeypair"
-  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
-  lifecycle {
-    ignore_changes = ["public_key"]
-  }
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCmionUf8CEl3zKwzyLeboyzHa3laXTR6+w386n9sOAo3I+N/V6o6DAAEVGtJFlbEDSyEJ1Nqlqjd7Ss+reP2nudjxJJ10seZWlStDgWuL+kVqy+w43GKF4xUcsGe9Bb2NnOzja1dlrbGfmqVZusA8wtg06HHJse89pp2M0DUMTV6MOQ7t8tXZIxgvHgniSBcGrEnPjX498bK4zxJ7GlyGyvI3zG7nsMemz1Ml+SgVTUVedql8veBcJpyyRhU5EpMT4hUmrJMTGrJakZQ5uoYsXSd1Y/0iar/US2ZNkfI+RCgFpaRnR/fiMtzSun+nH72phX/GSMItG6W0BXB/Tf+dbECWLQvBK9XYW9lI6idnYb0hxXWSievmc9pMFIOMEcqKVwj6vvPwkay3I1zxfSoz9sj6sMNoXdfFlz+AbcTuAKGAqMc4WLgiWtV3BD5MNOlcue5XDGBz/xhHfB60yjipIEBV0yynYc3mT132H2mvUlFLq6ZV6XLLH42wiHTYK52s= root@osboxes"
 }
